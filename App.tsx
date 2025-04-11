637c429e9265c5e8356979d6c140a46e7d5c3a14
rajat693 <rajatchaudhary.rc693@gmail.com>
2025-04-11 12:59:06 +0530

    fix: changed image rendering on screen

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Image source={require("./assets/icon.png")} />
+      <Image source={require("./assets/architecture.jpg")} />
     </View>
   );
 }
